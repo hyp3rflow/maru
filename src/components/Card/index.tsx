bdf900ac1258de1242d5448e338b23a30a09ef17
hyperflow <hyperflow@kakao.com>
2021-04-19 02:16:09 +0900

    fix(Card): Change color to use open-color

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import oc from 'open-color';
 
 const CardWrapper = styled.div`
   display: inline-flex;
   flex-direction: column;
-  padding: 24px;
-  background-color: rgb(255, 255, 255);
-  box-shadow: rgba(20, 20, 20, 2%) 2px 8px 12px 0px,
-    rgba(20, 20, 20, 2%) 0px 1px 3px 0px;
+  padding: 16px;
+  background-color: ${oc.white};
+  box-shadow: ${oc.gray[2]} 0px 1px 3px 0px;
   border-radius: 4px;
 `;
 
